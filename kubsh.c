835dcdefcb6891d9f065e9367dbd0abae9296900
Alina <anitalozova@gmail.com>
2025-09-20 13:55:25 +0300

    linux

--- kubsh.c
+++ kubsh.c
@@ -2,12 +2,29 @@
 #include <string.h>
 
 int main() {
+char input[100];
+while(1)
+{
 printf("$");
 
-char input[100];
-fgets(input, 100, stdin);
+if(fgets(input, 100, stdin)== NULL){
+printf("\nExit Cntr+D\n");
+break;
+}
 input[strlen(input) - 1] = '\0';
 
+if(strcmp(input, "exit" ) == 0){
+printf("exit from programm\n");
+break;}
+if(strcmp(input, "\\q" ) == 0){
+printf("exit from programm\\q\n");
+break;}
+if(strcmp(input, "echo" ) == 0){
+printf("123\n");
+break;}
+
+
 printf("%s: command not found\n",input);
+}
 return 0;
 }
