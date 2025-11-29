952fe6214a4db549193740176c347bc5b8084e68
Unamrr <anna12way@gmail.com>
2025-11-29 13:10:16 +0300

    Update mistake.c

--- mistake.c
+++ mistake.c
@@ -60,3 +60,26 @@ kubsh_image   latest    a3561f55db7b   2 weeks ago   152MB
 mynginx       latest    a3561f55db7b   2 weeks ago   152MB
 nginx         latest    d261fd19cb63   3 weeks ago   152MB
 hello-world   latest    d2c94e258dcb   2 years ago   13.3kB
+
+user@user-VirtualBox:~/docker$ docker run kubsh_image
+/docker-entrypoint.sh: /docker-entrypoint.d/ is not empty, will attempt to perform configuration
+/docker-entrypoint.sh: Looking for shell scripts in /docker-entrypoint.d/
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/10-listen-on-ipv6-by-default.sh
+10-listen-on-ipv6-by-default.sh: info: Getting the checksum of /etc/nginx/conf.d/default.conf
+10-listen-on-ipv6-by-default.sh: info: Enabled listen on IPv6 in /etc/nginx/conf.d/default.conf
+/docker-entrypoint.sh: Sourcing /docker-entrypoint.d/15-local-resolvers.envsh
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/20-envsubst-on-templates.sh
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/30-tune-worker-processes.sh
+/docker-entrypoint.sh: Configuration complete; ready for start up
+2025/11/29 10:07:01 [notice] 1#1: using the "epoll" event method
+2025/11/29 10:07:01 [notice] 1#1: nginx/1.29.3
+2025/11/29 10:07:01 [notice] 1#1: built by gcc 14.2.0 (Debian 14.2.0-19) 
+2025/11/29 10:07:01 [notice] 1#1: OS: Linux 6.8.0-45-generic
+2025/11/29 10:07:01 [notice] 1#1: getrlimit(RLIMIT_NOFILE): 1048576:1048576
+2025/11/29 10:07:01 [notice] 1#1: start worker processes
+2025/11/29 10:07:01 [notice] 1#1: start worker process 29
+2025/11/29 10:07:01 [notice] 1#1: start worker process 30
+2025/11/29 10:07:21 [notice] 1#1: signal 28 (SIGWINCH) received
+
+
+
